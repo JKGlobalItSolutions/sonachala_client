823a32f006618fbd38b033026697f33f78b0c1c5
sriram A <sriram.coder2004@gmail.com>
2025-06-09 15:57:31 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace with your repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/sonachala_client/',
 })
